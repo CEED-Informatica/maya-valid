a59ca5fa2e50ec6a6973a1d83d08b3a3d2746c30
Alfredo Oltra <aoltra@gmail.com>
2023-09-28 01:56:28 +0200

    feat: se añade la lista de convalidaciones

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import config_settings
+from . import student
 from . import validation
 from . import validation_subject
 from . import cron_register_jobs
---
+++ models/student.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+class Student(models.Model):
+  """
+  Ejemplo de herencia de clase
+  Herencia del modelo maya_core.student
+  Lo que hace es modificar el modelo maya_core.student para incluir el campo maya_employee_id, que
+  se añadirá a la tabla maya_core.student de la base de datos
+  Ese campo es accesible por cualquier otro módulo
+  """
+  _inherit = 'maya_core.student'
+
+  # un estudiante podría solicitar convalidaciones de dos ciclos diferentes 
+  # (aunque a día de hoy no está permitido)
+  validations_ids = fields.One2many('maya_valid.validation', 'student_id')
