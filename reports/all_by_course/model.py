83692723d1a7c0531867d0ae4bf11f8b6c54a840
Alfredo Oltra <aoltra@gmail.com>
2024-06-25 20:57:53 +0000

    feat: soporte para la descarga de varios infomes

--- reports/all_by_course/model.py
+++ reports/all_by_course/model.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, tools
 import os
 import base64
-from odoo.exceptions import ValidationError
+import shutil
+from datetime import datetime
 
 class WizardReportAllByCourse(models.TransientModel):
   """
@@ -27,15 +28,43 @@ class WizardReportAllByCourse(models.TransientModel):
     else:
       courses = self.courses_ids 
       
+    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+    addons_path = max([path for path in tools.config['addons_path'].split(',') if path in os.path.dirname(__file__)], key = len)
+
+    if len(courses) == 1:
+      now = ''
+    else:
+       os.makedirs(os.path.join(addons_path, 'maya_core','tmp_files', 'reports', now))
+
     for course in courses:
       validations = self.env['maya_valid.validation'].search([('course_id', '=', course.id), ('state', '=', 14)], order = 'student_surname asc')
+
       pdf = self.env.ref('maya_valid.validations_pdf_report')._render_qweb_pdf([val.id for val in validations])[0]
       data = base64.encodestring(pdf)
    
-      #sss = os.path.join(os.path.dirname(__file__), f'{course.abbr}.pdf')
-      kdb = open(os.path.join(os.path.dirname(__file__), f'{course.abbr}.pdf'), 'wb')
+      path_tmp = os.path.join(addons_path, 'maya_core/tmp_files/reports', now, f'Convalidaciones_cerradas_{course.abbr}.pdf')
+      kdb = open(path_tmp, 'wb')
       kdb.write(pdf)
       kdb.close()
+
+    base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+    
+      
+    if len(courses) > 1:
+      path_tmp = os.path.join(addons_path, 'maya_core/tmp_files/reports', now)
+      zip_name = os.path.join(f'{addons_path}/maya_core/static/reports', f'Convalidaciones_cerradas_{now}')
+      shutil.make_archive(zip_name, 'zip', path_tmp)
+      url = f'{base_url}/maya_core/static/reports/Convalidaciones_cerradas_{now}.zip'
+    else:
+      filename = os.path.join(f'{addons_path}/maya_core/static/reports', f'Convalidaciones_cerradas_{courses[0].abbr}.pdf')
+      shutil.copy(path_tmp, filename)
+      url = f'{base_url}/maya_core/static/reports/Convalidaciones_cerradas_{courses[0].abbr}.pdf'
+
+    return {
+      'type': 'ir.actions.act_url',
+      'target': 'self',
+      'url': url
+    }
 
   """   
         data = {}
