96db71899d9c8ae63b6641dfb0f5fcd01160f2d4
Alfredo Oltra <aoltra@gmail.com>
2024-07-23 07:40:09 +0000

    feat: añade la comprobación de las convalidacions por experiencia profesional

--- models/cron_register_jobs/cron_job_check_deadline_validations.py
+++ models/cron_register_jobs/cron_job_check_deadline_validations.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, api
-from datetime import date, timedelta, datetime
+from datetime import date
 import logging
 
 _logger = logging.getLogger(__name__)
 
-class CronJobCheckDeadlineValidations(models.TransientModel):
-  _name = 'maya_valid.cron_job_check_deadline_validations'
+class CronJobCheckDeadlineStudiesValidations(models.TransientModel):
+  _name = 'maya_valid.cron_job_check_deadline_studies_validations'
   
   @api.model
   def cron_check_deadline_validations(self):
     today = date.today()
-    validations = self.env['maya_valid.validation'].search([])
+   
+    # convalidaciones por estudios
+    validations = self.env['maya_valid.studies_validation'].search([])
 
     updated_validations = ''
     for val in validations:
@@ -22,5 +24,18 @@ class CronJobCheckDeadlineValidations(models.TransientModel):
         })
         updated_validations += f'{val["course_abbr"]}/studend_id: {val["student_id"].id},'
 
+    _logger.info('Subsanaciones por estudios fuera de plazo: '+ updated_validations)
 
-    _logger.info('Subsanaciones fuera de plazo: '+ updated_validations)
+    # convalidaciones por experiencia profesional
+    validations = self.env['maya_valid.competency_validation'].search([])
+
+    updated_validations = ''
+    for val in validations:
+      if val["correction_date_end"] != False and today > val["correction_date_end"]:
+        val.write({
+          'situation': '4'  
+        })
+        updated_validations += f'{val["course_abbr"]}/studend_id: {val["student_id"].id},'
+
+    _logger.info('Subsanaciones por acreditación de competencias fuera de plazo: '+ updated_validations)
+
